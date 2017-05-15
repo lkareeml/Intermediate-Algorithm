4864e060321eb3079640662881117c1a3b15df2d
kareem atef <kareem.atef@yahoo.com>
2017-05-15 23:39:32 +0200

    Create 009-Boo-who.js

---
+++ 009-Boo-who.js
@@ -0,0 +1,14 @@
+//Boo who
+/*
+Check if a value is classified as a boolean primitive. Return true or false.
+Boolean primitives are true and false.
+*/
+function booWho(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+  if (bool===true||bool===false)
+    return true;
+  else 
+    return false;
+}
+
+booWho(null);
