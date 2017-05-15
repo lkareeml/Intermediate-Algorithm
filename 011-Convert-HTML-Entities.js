6237f9d17db01676068c84961e7f5c30e9256188
kareem atef <kareem.atef@yahoo.com>
2017-05-15 23:59:55 +0200

    Create 011-Convert-HTML-Entities.js

---
+++ 011-Convert-HTML-Entities.js
@@ -0,0 +1,14 @@
+//Convert HTML Entities 
+
+/*Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.*/
+
+function convertHTML(str) {
+  str = str.replace(/\&/g,"&amp;");
+  str = str.replace(/</g,"&lt;");
+  str = str.replace(/>/g,"&gt;");
+  str = str.replace(/\"/g,"&quot;");
+  str = str.replace(/\'/g,"&apos;");
+  return str;
+}
+
+convertHTML("Dolce & Gabbana");
